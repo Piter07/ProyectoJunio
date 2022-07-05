3185f70a7e93594ae2654784f26e2772b222758d
Piter07 <verdedrope99>
2022-07-05 10:56:27 -0400

    Subo los cambios que hice en el dia

---
+++ src/test/java/com/roshka/bootcamp/ProyectoJunio/ProyectoJunioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roshka.bootcamp.ProyectoJunio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoJunioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
