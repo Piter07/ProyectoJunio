c0c7e3ebd608f1126c8a49f9864cf717476848a5
oscar <oscaren_gonz0912@hotmail.com>
2022-07-11 13:10:30 -0400

    [ADD] creacion albumDTO (parcial)

---
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/dto/AlbumDTO.java
@@ -0,0 +1,14 @@
+package com.roshka.bootcamp.ProyectoJunio.controller.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class AlbumDTO {
+    private String titulo;
+    private String descripcion;
+    private Long idCategoria;
+    private Date fechaEvento;
+//    private Long idUsuario;
+}
