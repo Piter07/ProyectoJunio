ee603fc27ad642cdee6a15e7c81651e83ef8487d
dani <daniel.dossantos.646@fpuna.edu.py>
2022-07-12 11:09:47 -0400

    algunas correciones de diseño

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/AlbumController.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/AlbumController.java
@@ -7,10 +7,8 @@ import com.roshka.bootcamp.ProyectoJunio.service.AlbumService;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
@@ -56,9 +54,8 @@ public class AlbumController {
         return "formulario-fotos";
     }
     @PostMapping("/creacion-album")
-    public String postFormFotos(@ModelAttribute("album") AlbumDTO albumDTO){
+    public String postFormFotos(@ModelAttribute("album") AlbumDTO albumDTO, @RequestParam("file") MultipartFile multipartFile){
         albumService.guardar(albumDTO);
-        System.out.println("Llegaron los datos");
         return "formulario-fotos";
     }
 }
