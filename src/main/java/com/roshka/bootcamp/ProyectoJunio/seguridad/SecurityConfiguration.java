ce0884043b9fd4e3d637cbb49403f891dffd398f
dani <daniel.dossantos.646@fpuna.edu.py>
2022-07-14 09:36:12 -0400

    preparando para separar los formularios

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/seguridad/SecurityConfiguration.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/seguridad/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/home", "/index", "/registro", "/css/**","/js/**","/img/**")
+                    .antMatchers("/**", "/home", "/index", "/registro", "/css/**","/js/**","/img/**")
                     .permitAll()
                     .anyRequest()
                     .authenticated()
