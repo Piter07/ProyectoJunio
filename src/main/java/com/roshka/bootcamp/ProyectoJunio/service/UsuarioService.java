68f4cc514c8c1aad9d4aee94729c74c17e20c8dd
Alexis Ortiz <olmedo.aortiz@gmail.com>
2022-07-15 00:09:39 -0400

    [MOD] Funcionalidad de Login corregido y testeado correctamente.

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/service/UsuarioService.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/service/UsuarioService.java
@@ -17,11 +17,12 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class UsuarioService implements UsuarioServiceInterface {
