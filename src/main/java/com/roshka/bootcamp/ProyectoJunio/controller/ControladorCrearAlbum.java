0dc2f0340b1735f376e0eaac8cc1ae9309a4b19f
oscar <oscaren_gonz0912@hotmail.com>
2022-07-15 14:55:30 -0400

    [MOD] se agrega ruta para subir fotos

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/ControladorCrearAlbum.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/ControladorCrearAlbum.java
@@ -9,9 +9,15 @@ import com.roshka.bootcamp.ProyectoJunio.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 @Controller
 public class ControladorCrearAlbum {
@@ -33,6 +39,35 @@ public class ControladorCrearAlbum {
         Album albumGuardado = albumService.guardar(albumDTO);
         albumDTO.setId_album(albumGuardado.getId_album());
         System.out.println("Creamos un album exitosamente");
+        return "redirect:/album/" + albumGuardado.getId_album() + "/subir-fotos";
+    }
+
+    @GetMapping("/album/{id}/subir-fotos")
+    public String subirFotos(@PathVariable long id, Model model) throws Exception {
+        Optional<Album> albumActual = albumService.findById(id);
+        if (albumActual.isPresent()) {
+            model.addAttribute("album", albumActual.get());
+        }
         return "DropzoneFoto";
     }
+
+    @PostMapping("/album/{id}/subir-fotos")
+    public String postFormFoto(@RequestParam("file") MultipartFile file, @PathVariable long id){
+        Optional<Album> albumActual = albumService.findById(id);
+        if (albumActual.isPresent()) {
+            if(!file.isEmpty()){
+                Path directorioImagenes= Paths.get("src/main/resources/media/"+ albumActual.get().getId_album() + file.getOriginalFilename());
+                String rutaAbsoluta=directorioImagenes.toFile().getAbsolutePath();
+                try {
+                    byte[] bytesImg=file.getBytes();
+                    Path rutaCompleta=Paths.get(rutaAbsoluta+"/"+file.getOriginalFilename());
+                    Files.write(rutaCompleta,bytesImg);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return "redirect:album/" + albumActual.get().getId_album() ;
+    }
+
 }
