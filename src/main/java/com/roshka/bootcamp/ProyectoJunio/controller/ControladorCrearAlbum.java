0853e56d789d05967de699609ed81f8c035b05e0
oscar <oscaren_gonz0912@hotmail.com>
2022-07-19 13:13:47 -0400

    [MOD] fix cuando se guarda la foto del archivo

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/ControladorCrearAlbum.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/ControladorCrearAlbum.java
@@ -7,6 +7,7 @@ import com.roshka.bootcamp.ProyectoJunio.model.Usuario;
 import com.roshka.bootcamp.ProyectoJunio.service.AlbumService;
 import com.roshka.bootcamp.ProyectoJunio.service.FotoService;
 import com.roshka.bootcamp.ProyectoJunio.service.UsuarioService;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -58,18 +59,19 @@ public class ControladorCrearAlbum {
 
     @PostMapping("/album/{id}/subir-fotos")
     public String postFormFoto(@RequestParam("file") MultipartFile file, @PathVariable long id, Model model){
+        final String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
         Optional<Album> albumActual = albumService.findById(id);
         model.addAttribute("idAlbum", id);
         if (albumActual.isPresent()) {
             if(!file.isEmpty()){
-                Path directorioImagenes= Paths.get("src/main/resources/public/"+  file.getOriginalFilename());
+                Path directorioImagenes= Paths.get("src/main/resources/public/"+ DigestUtils.md5Hex(file.getOriginalFilename()));
                 String rutaAbsoluta = directorioImagenes.toFile().getAbsolutePath();
                 try {
                     byte[] bytesImg=file.getBytes();
                     Path rutaCompleta=Paths.get(rutaAbsoluta);
                     Files.write(rutaCompleta, bytesImg);
 //                     si todo salio bien guardamos la foto en la base de datos
-                    fotoService.guardarFoto(albumActual.get(), file.getOriginalFilename());
+                    fotoService.guardarFoto(albumActual.get(), DigestUtils.md5Hex(file.getOriginalFilename()));
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
